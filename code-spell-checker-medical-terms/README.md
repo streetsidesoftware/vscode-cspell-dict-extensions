d4894047fa516748c556f56f8692ebe99a9de3ad
Jason Dent <jason@streetsidesoftware.nl>
2017-08-07 22:57:15 +0200

    Add medical terms extension

---
+++ code-spell-checker-medical-terms/README.md
@@ -0,0 +1,35 @@
+# Medical Terms for Code Spell Checker
+
+Imports 90000+ Medical Terms for [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker).
+
+## [List of English Medical Terms](https://github.com/Glutanimate/wordlist-medicalterms-en)
+
+Source: https://github.com/Glutanimate/wordlist-medicalterms-en
+
+### Overview of Word List
+
+This is a simple list of English medical terms formatted as a UTF8-encoded text file. It is based on two prominent medical dictionary projects:
+
+ - [OpenMedSpel](http://www.e-medtools.com/openmedspel.html) by e-MedTools
+ - [Raj&Co-Med-Spel-Chek by Rajasekharan N. of Raj&Co](http://rajn.co/free-medical-spell-checker-for-microsoft-word-custom-dictionary/)
+
+The two sources have been merged, deduplicated, corrected and formatted as a text file that should be compatible with Android dictionary managers, LibreOffice, and Word.
+
+### Details
+
+    Terms:              98119
+    Contents:           drug names (up-to-date with FDA-approvals as of 2014-04-02, trade and generic names),
+                        anatomical terms, dermatological terms, internal medicine terms, surgical terms,
+                        DSM-IV terms, ICD-9 terms, and many more
+    Author:             (c) 2014-2017 Aristotelis P. (https://github.com/Glutanimate)
+    Original Authors:   (c) 2007-2014 R. Robinson <info@e-medtools.com>,
+                        (c) 2009-2014 Rajasekharan N. <https://plus.google.com/u/0/+Rajasekharan-N/>
+    Original Sources:   - OpenMedSpel by R. Robinson of e-MedTools (Version 2.0.0, released 2014-01-21)
+                          <http://www.e-medtools.com/openmedspel.html>
+                        - MTH-Med-Spel-Chek by Rajasekharan N. of MT-Herald (released 2014-04-02)
+                          <http://mtherald.com/free-medical-spell-checker-for-microsoft-word-custom-dictionary/>
+    License:            GNU GPL v3 (see LICENSEs for more information)
+
+
+## Requirements
+This extension will automatically include [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker) extension.
---
+++ code-spell-checker-medical-terms/src/extension.ts
@@ -0,0 +1,55 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as dict from 'cspell-dict-es-es';
+
+interface CodeSpellCheckerExtension {
+    registerConfig(path: string): Promise<void>;
+    enableLocal(isGlobal: boolean, local: string): Promise<void>;
+    disableLocal(isGlobal: boolean, local: string): Promise<void>;
+}
+
+const local = 'es';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
+
+    const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
+
+    if (extension) {
+        extension.activate().then(ext => {
+            const path = dict.getConfigLocation();
+            ext && ext.registerConfig && ext.registerConfig(path);
+        });
+    }
+
+    function enableSpanish(isGlobal: boolean) {
+        extension && extension.activate().then(ext => {
+            ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
+        });
+    }
+
+    function disableSpanish(isGlobal: boolean) {
+        extension && extension.activate().then(ext => {
+            ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
+        });
+    }
+
+    // Push the disposable to the context's subscriptions so that the
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(
+        vscode.commands.registerCommand('cSpell.enableSpanish', () => enableSpanish(true)),
+        vscode.commands.registerCommand('cSpell.disableSpanish', () => disableSpanish(true)),
+        vscode.commands.registerCommand('cSpell.enableSpanishWorkspace', () => enableSpanish(false)),
+        vscode.commands.registerCommand('cSpell.disableSpanishWorkspace', () => disableSpanish(false)),
+    );
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
