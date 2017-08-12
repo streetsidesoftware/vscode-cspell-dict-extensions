38c98dabfcdb0bd033a77727671eabd78814270e
Jason Dent <jason@streetsidesoftware.nl>
2017-08-12 13:31:33 +0200

    Remove Unused code.

--- generator-cspell-dicts-extensions/generators/app/templates/src/extension.ts
+++ generator-cspell-dicts-extensions/generators/app/templates/src/extension.ts
@@ -10,11 +10,9 @@ interface CodeSpellCheckerExtension {
     disableLocal(isGlobal: boolean, local: string): Promise<void>;
 }
 
-const local = 'es';
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(/*context: vscode.ExtensionContext*/) {
 
     const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
 
@@ -23,31 +21,10 @@ export function activate(context: vscode.ExtensionContext) {
     if (extension) {
         extension.activate().then(ext => {
             const path = dict.getConfigLocation();
+            // We need to register the dictionary configuration with the Code Spell Checker Extension
             ext && ext.registerConfig && ext.registerConfig(path);
         });
     }
-
-    function enableSpanish(isGlobal: boolean) {
-        extension && extension.activate().then(ext => {
-            ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
-        });
-    }
-
-    function disableSpanish(isGlobal: boolean) {
-        extension && extension.activate().then(ext => {
-            ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
-        });
-    }
-
-    // Push the disposable to the context's subscriptions so that the
-    // client can be deactivated on extension deactivation
-    context.subscriptions.push(
-        vscode.commands.registerCommand('cSpell.enableSpanish', () => enableSpanish(true)),
-        vscode.commands.registerCommand('cSpell.disableSpanish', () => disableSpanish(true)),
-        vscode.commands.registerCommand('cSpell.enableSpanishWorkspace', () => enableSpanish(false)),
-        vscode.commands.registerCommand('cSpell.disableSpanishWorkspace', () => disableSpanish(false)),
-    );
-
 }
 
 // this method is called when your extension is deactivated
