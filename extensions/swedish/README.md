886e8b32da255f1161cbbbd8f4efd8ad7aeb2cc4
Jason Dent <jason@streetsidesoftware.nl>
2017-08-12 18:41:20 +0200

    Add Swedish

---
+++ extensions/swedish/README.md
@@ -0,0 +1,41 @@
+# Swedish - Code Spell Checker
+
+Swedish dictionary extension for VS Code.
+
+Imports the Swedish spell checking dictionary for [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker).
+
+
+
+## Installation
+
+After this extension is installed, it is necessary to tell the spell checker to use it.
+
+### Enable Dictionary
+
+Commands (use `F1` or *View -> Command Palette...*):
+- `Enable Swedish Spell Checker Dictionary`
+- `Enable Swedish Spell Checker Dictionary in Workspace`
+
+
+### Disable Dictionary
+
+Commands (use `F1` or *View -> Command Palette...*):
+- `Disable Swedish Spell Checker Dictionary`
+- `Disable Swedish Spell Checker Dictionary in Workspace`
+
+### Manual Settings
+
+This is done with the `language` setting.
+
+*Preferences* -> *Settings*
+
+Adding `sv` to the `cSpell.language` setting, will enable the Spanish dictionary.
+Example using both English and Swedish dictionaries:
+```javascript
+"cSpell.language": "en,sv",
+```
+
+
+
+## Requirements
+This extension will automatically include [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker) extension.
---
+++ extensions/swedish/src/extension.ts
@@ -0,0 +1,59 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as dict from 'cspell-dict-sv';
+
+interface CodeSpellCheckerExtension {
+    registerConfig(path: string): Promise<void>;
+    enableLocal(isGlobal: boolean, local: string): Promise<void>;
+    disableLocal(isGlobal: boolean, local: string): Promise<void>;
+}
+
+
+const local = 'sv';
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
+
+    const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
+
+    if (extension) {
+        extension.activate().then(ext => {
+            const path = dict.getConfigLocation();
+            // We need to register the dictionary configuration with the Code Spell Checker Extension
+            ext && ext.registerConfig && ext.registerConfig(path);
+        });
+    }
+
+    
+    function enableSwedish(isGlobal: boolean) {
+        extension && extension.activate().then(ext => {
+            ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
+        });
+    }
+
+    function disableSwedish(isGlobal: boolean) {
+        extension && extension.activate().then(ext => {
+            ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
+        });
+    }
+
+    // Push the disposable to the context's subscriptions so that the
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(
+        vscode.commands.registerCommand('cSpellExt_swedish.enableSwedish', () => enableSwedish(true)),
+        vscode.commands.registerCommand('cSpellExt_swedish.disableSwedish', () => disableSwedish(true)),
+        vscode.commands.registerCommand('cSpellExt_swedish.enableSwedishWorkspace', () => enableSwedish(false)),
+        vscode.commands.registerCommand('cSpellExt_swedish.disableSwedishWorkspace', () => disableSwedish(false)),
+    );
+    
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
