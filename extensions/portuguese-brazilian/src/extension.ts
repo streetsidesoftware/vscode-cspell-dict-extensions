b72eaa8d72e8e4cda098da58256376a9c1a93fda
Jason Dent <Jason3S@users.noreply.github.com>
2021-09-03 23:19:58 +0200

    dev: Upgrade to the Latest Brazilian Dictionary (#137)
    * dev: Upgrade to the Latest Brazilian Dictionary
    * Update License to match dictionary.
    * Update package.json

--- extensions/portuguese-brazilian/src/extension.ts
+++ extensions/portuguese-brazilian/src/extension.ts
@@ -1,59 +1,61 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as dict from 'cspell-dict-pt-br';
 
 interface CodeSpellCheckerExtension {
     registerConfig(path: string): Promise<void>;
-    enableLocal(isGlobal: boolean, local: string): Promise<void>;
-    disableLocal(isGlobal: boolean, local: string): Promise<void>;
+    enableLocal(isGlobal: boolean, locale: string): Promise<void>;
+    disableLocal(isGlobal: boolean, locale: string): Promise<void>;
 }
 
-
-const local = 'pt,pt_BR';
-
+const locale = 'pt,pt_BR';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
+    const configLocation = context.asAbsolutePath('./cspell-ext.json');
 
     const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
 
     if (extension) {
-        extension.activate().then(ext => {
-            const path = dict.getConfigLocation();
+        extension.activate().then((ext) => {
             // We need to register the dictionary configuration with the Code Spell Checker Extension
-            ext && ext.registerConfig && ext.registerConfig(path);
+            ext?.registerConfig?.(configLocation);
         });
     }
 
-    
-    function enablePortuguese_brazilian(isGlobal: boolean) {
-        extension && extension.activate().then(ext => {
-            ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
-        });
+    function enable(isGlobal: boolean) {
+        extension &&
+            extension.activate().then((ext) => {
+                ext?.enableLocal?.(isGlobal, locale);
+            });
     }
 
-    function disablePortuguese_brazilian(isGlobal: boolean) {
-        extension && extension.activate().then(ext => {
-            ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
-        });
+    function disable(isGlobal: boolean) {
+        extension &&
+            extension.activate().then((ext) => {
+                ext?.disableLocal?.(isGlobal, locale);
+            });
     }
 
     // Push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
     context.subscriptions.push(
-        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.enablePortuguese_brazilian', () => enablePortuguese_brazilian(true)),
-        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.disablePortuguese_brazilian', () => disablePortuguese_brazilian(true)),
-        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.enablePortuguese_brazilianWorkspace', () => enablePortuguese_brazilian(false)),
-        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.disablePortuguese_brazilianWorkspace', () => disablePortuguese_brazilian(false)),
+        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.enablePortuguese_brazilian', () =>
+            enable(true)
+        ),
+        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.disablePortuguese_brazilian', () =>
+            disable(true)
+        ),
+        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.enablePortuguese_brazilianWorkspace', () =>
+            enable(false)
+        ),
+        vscode.commands.registerCommand('cSpellExt_portuguese-brazilian.disablePortuguese_brazilianWorkspace', () =>
+            disable(false)
+        )
     );
-    
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
