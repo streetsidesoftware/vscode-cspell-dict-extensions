ce0e7b40df462b225b47d0bbdb8eef459eb1c868
Jason Dent <jason@streetsidesoftware.nl>
2022-08-12 16:20:15 +0200

    fix: Add root the root to the packages.

--- scripts/gen-release-please-config.sh
+++ scripts/gen-release-please-config.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 JQ_FILTER_PACKAGES="$SCRIPT_DIR/gen-release-please-config-packages.jq"
 
-PACKAGES="$(echo $(ls -1 extensions/*/package.json) \
+PACKAGES="$(echo ./package.json $(ls -1 extensions/*/package.json) \
     | xargs jq -f $JQ_FILTER_PACKAGES | jq -s "add | { packages: . }")"
 
 
