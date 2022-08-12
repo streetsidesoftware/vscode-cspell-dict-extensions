ce1a54b92f9fe4030bae7e3036baf43c2a8ca6e2
Jason Dent <Jason3S@users.noreply.github.com>
2022-08-12 12:48:26 +0200

    feat!: Update Danish dictionary to v2 (#350)
    * feat!: Update Danish dictionary to v2
    * Fix the Danish language code and add a sample file.
      Breaking change, the language code is now `da` instead of `dk`.

--- extensions/danish/src/extension.ts
+++ extensions/danish/src/extension.ts
@@ -2,53 +2,55 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as dict from 'cspell-dict-da-dk';
-
 interface CodeSpellCheckerExtension {
     registerConfig(path: string): Promise<void>;
-    enableLocal(isGlobal: boolean, local: string): Promise<void>;
-    disableLocal(isGlobal: boolean, local: string): Promise<void>;
+    enableLocale(isGlobal: boolean, locale: string): Promise<void>;
+    disableLocale(isGlobal: boolean, locale: string): Promise<void>;
 }
 
-const local = 'dk';
+//
+const locale = 'da';
+//
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
+    const configLocation = context.asAbsolutePath('./cspell-ext.json');
 
     const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
 
     if (extension) {
         extension.activate().then((ext) => {
-            const path = dict.getConfigLocation();
             // We need to register the dictionary configuration with the Code Spell Checker Extension
-            ext && ext.registerConfig && ext.registerConfig(path);
+            ext?.registerConfig?.(configLocation);
         });
     }
 
-    function enableDanish(isGlobal: boolean) {
+    //
+    function enable(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
+                ext?.enableLocale?.(isGlobal, locale);
             });
     }
 
-    function disableDanish(isGlobal: boolean) {
+    function disable(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
+                ext?.disableLocale?.(isGlobal, locale);
             });
     }
 
     // Push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
     context.subscriptions.push(
-        vscode.commands.registerCommand('cSpellExt_danish.enableDanish', () => enableDanish(true)),
-        vscode.commands.registerCommand('cSpellExt_danish.disableDanish', () => disableDanish(true)),
-        vscode.commands.registerCommand('cSpellExt_danish.enableDanishWorkspace', () => enableDanish(false)),
-        vscode.commands.registerCommand('cSpellExt_danish.disableDanishWorkspace', () => disableDanish(false))
+        vscode.commands.registerCommand('cSpellExt_danish.enableDanish', () => enable(true)),
+        vscode.commands.registerCommand('cSpellExt_danish.disableDanish', () => disable(true)),
+        vscode.commands.registerCommand('cSpellExt_danish.enableDanishWorkspace', () => enable(false)),
+        vscode.commands.registerCommand('cSpellExt_danish.disableDanishWorkspace', () => disable(false))
     );
+    //
 }
 
 // this method is called when your extension is deactivated
