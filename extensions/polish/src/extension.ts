8103250c90d033df7b941677a06f61cd43aaf371
Jason Dent <jason@streetsidesoftware.nl>
2022-08-12 19:17:59 +0200

    Revert "feat!: Update the Polish Dictionary to v2"
    This reverts commit 75924c4c815c9671c5017123b661511d5cf0d718.

--- extensions/polish/src/extension.ts
+++ extensions/polish/src/extension.ts
@@ -2,55 +2,53 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as dict from 'cspell-dict-pl_pl';
+
 interface CodeSpellCheckerExtension {
     registerConfig(path: string): Promise<void>;
-    enableLocale(isGlobal: boolean, locale: string): Promise<void>;
-    disableLocale(isGlobal: boolean, locale: string): Promise<void>;
+    enableLocal(isGlobal: boolean, local: string): Promise<void>;
+    disableLocal(isGlobal: boolean, local: string): Promise<void>;
 }
 
-//
-const locale = 'pl';
-//
+const local = 'pl';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
-    const configLocation = context.asAbsolutePath('./cspell-ext.json');
 
     const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
 
     if (extension) {
         extension.activate().then((ext) => {
+            const path = dict.getConfigLocation();
             // We need to register the dictionary configuration with the Code Spell Checker Extension
-            ext?.registerConfig?.(configLocation);
+            ext && ext.registerConfig && ext.registerConfig(path);
         });
     }
 
-    //
-    function enable(isGlobal: boolean) {
+    function enablePolish(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext?.enableLocale?.(isGlobal, locale);
+                ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
             });
     }
 
-    function disable(isGlobal: boolean) {
+    function disablePolish(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext?.disableLocale?.(isGlobal, locale);
+                ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
             });
     }
 
     // Push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
     context.subscriptions.push(
-        vscode.commands.registerCommand('cSpellExt_polish.enable', () => enable(true)),
-        vscode.commands.registerCommand('cSpellExt_polish.disable', () => disable(true)),
-        vscode.commands.registerCommand('cSpellExt_polish.enableWorkspace', () => enable(false)),
-        vscode.commands.registerCommand('cSpellExt_polish.disableWorkspace', () => disable(false))
+        vscode.commands.registerCommand('cSpellExt_polish.enablePolish', () => enablePolish(true)),
+        vscode.commands.registerCommand('cSpellExt_polish.disablePolish', () => disablePolish(true)),
+        vscode.commands.registerCommand('cSpellExt_polish.enablePolishWorkspace', () => enablePolish(false)),
+        vscode.commands.registerCommand('cSpellExt_polish.disablePolishWorkspace', () => disablePolish(false))
     );
-    //
 }
 
 // this method is called when your extension is deactivated
