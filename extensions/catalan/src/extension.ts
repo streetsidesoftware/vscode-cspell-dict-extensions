7e16e1f7015bc8bdc9c4fddfab93392834db460a
Jason Dent <Jason3S@users.noreply.github.com>
2022-08-12 18:13:36 +0200

    feat!: Upgrade Catalan Dictionary to v2 (#364)

--- extensions/catalan/src/extension.ts
+++ extensions/catalan/src/extension.ts
@@ -2,53 +2,55 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as dict from 'cspell-dict-ca';
-
 interface CodeSpellCheckerExtension {
     registerConfig(path: string): Promise<void>;
-    enableLocal(isGlobal: boolean, local: string): Promise<void>;
-    disableLocal(isGlobal: boolean, local: string): Promise<void>;
+    enableLocale(isGlobal: boolean, locale: string): Promise<void>;
+    disableLocale(isGlobal: boolean, locale: string): Promise<void>;
 }
 
-const local = 'ca';
+//
+const locale = 'ca';
+//
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
+    const configLocation = context.asAbsolutePath('./cspell-ext.json');
 
     const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
 
     if (extension) {
         extension.activate().then((ext) => {
-            const path = dict.getConfigLocation();
             // We need to register the dictionary configuration with the Code Spell Checker Extension
-            ext && ext.registerConfig && ext.registerConfig(path);
+            ext?.registerConfig?.(configLocation);
         });
     }
 
-    function enableCatalan(isGlobal: boolean) {
+    //
+    function enable(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
+                ext?.enableLocale?.(isGlobal, locale);
             });
     }
 
-    function disableCatalan(isGlobal: boolean) {
+    function disable(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
+                ext?.disableLocale?.(isGlobal, locale);
             });
     }
 
     // Push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
     context.subscriptions.push(
-        vscode.commands.registerCommand('cSpellExt_catalan.enableCatalan', () => enableCatalan(true)),
-        vscode.commands.registerCommand('cSpellExt_catalan.disableCatalan', () => disableCatalan(true)),
-        vscode.commands.registerCommand('cSpellExt_catalan.enableCatalanWorkspace', () => enableCatalan(false)),
-        vscode.commands.registerCommand('cSpellExt_catalan.disableCatalanWorkspace', () => disableCatalan(false))
+        vscode.commands.registerCommand('cSpellExt_catalan.enableCatalan', () => enable(true)),
+        vscode.commands.registerCommand('cSpellExt_catalan.disableCatalan', () => disable(true)),
+        vscode.commands.registerCommand('cSpellExt_catalan.enableCatalanWorkspace', () => enable(false)),
+        vscode.commands.registerCommand('cSpellExt_catalan.disableCatalanWorkspace', () => disable(false))
     );
+    //
 }
 
 // this method is called when your extension is deactivated
