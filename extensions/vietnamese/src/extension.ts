6c09bcf9f1e68b203c8cb40d3c45c7218d55a328
Jason Dent <Jason3S@users.noreply.github.com>
2022-08-13 08:07:58 +0200

    feat!: Update Vietnamese Dictionary to v2 (#374)
    Related to #352

--- extensions/vietnamese/src/extension.ts
+++ extensions/vietnamese/src/extension.ts
@@ -2,57 +2,55 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as dict from 'cspell-dict-vi-vn';
-
 interface CodeSpellCheckerExtension {
     registerConfig(path: string): Promise<void>;
-    enableLocal(isGlobal: boolean, local: string): Promise<void>;
-    disableLocal(isGlobal: boolean, local: string): Promise<void>;
+    enableLocale(isGlobal: boolean, locale: string): Promise<void>;
+    disableLocale(isGlobal: boolean, locale: string): Promise<void>;
 }
 
-const local = 'vi';
+//
+const locale = 'vi';
+//
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
+    const configLocation = context.asAbsolutePath('./cspell-ext.json');
 
     const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
 
     if (extension) {
         extension.activate().then((ext) => {
-            const path = dict.getConfigLocation();
             // We need to register the dictionary configuration with the Code Spell Checker Extension
-            ext && ext.registerConfig && ext.registerConfig(path);
+            ext?.registerConfig?.(configLocation);
         });
     }
 
-    function enableVietnamese(isGlobal: boolean) {
+    //
+    function enable(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext && ext.enableLocal && ext.enableLocal(isGlobal, local);
+                ext?.enableLocale?.(isGlobal, locale);
             });
     }
 
-    function disableVietnamese(isGlobal: boolean) {
+    function disable(isGlobal: boolean) {
         extension &&
             extension.activate().then((ext) => {
-                ext && ext.disableLocal && ext.disableLocal(isGlobal, local);
+                ext?.disableLocale?.(isGlobal, locale);
             });
     }
 
     // Push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
     context.subscriptions.push(
-        vscode.commands.registerCommand('cSpellExt_vietnamese.enableVietnamese', () => enableVietnamese(true)),
-        vscode.commands.registerCommand('cSpellExt_vietnamese.disableVietnamese', () => disableVietnamese(true)),
-        vscode.commands.registerCommand('cSpellExt_vietnamese.enableVietnameseWorkspace', () =>
-            enableVietnamese(false)
-        ),
-        vscode.commands.registerCommand('cSpellExt_vietnamese.disableVietnameseWorkspace', () =>
-            disableVietnamese(false)
-        )
+        vscode.commands.registerCommand('cSpellExt_vietnamese.enable', () => enable(true)),
+        vscode.commands.registerCommand('cSpellExt_vietnamese.disable', () => disable(true)),
+        vscode.commands.registerCommand('cSpellExt_vietnamese.enableWorkspace', () => enable(false)),
+        vscode.commands.registerCommand('cSpellExt_vietnamese.disableWorkspace', () => disable(false))
     );
+    //
 }
 
 // this method is called when your extension is deactivated
