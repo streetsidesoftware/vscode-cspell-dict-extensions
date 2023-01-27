96009cce0a9a47f5149ea3feb3b08724bd8ac29c
Jason Dent <jason@streetsidesoftware.nl>
2023-01-27 21:31:01 +0100

    chore: remove dependency upon mkdirp

--- generator-cspell-dicts-extensions/generators/app/index.js
+++ generator-cspell-dicts-extensions/generators/app/index.js
@@ -2,165 +2,162 @@
 const Generator = require('yeoman-generator');
 const yosay = require('yosay');
 const path = require('path');
-const mkdirp = require('mkdirp');
+const { promises: fs } = require('fs');
 
 const genName = 'generator-cspell-dicts-extensions';
 
+function mkdirp(file) {
+    return fs.mkdir(file, { recursive: true });
+}
+
 module.exports = class extends Generator {
-  constructor(args, opts) {
-    super(args, opts);
+    constructor(args, opts) {
+        super(args, opts);
 
-    this.argument('name', {
-      desc: 'Name of Dictionary',
-      type: String,
-      required: true
-    });
-  }
-
-  async initializing() {
-    if (path.basename(this.destinationPath()) === genName) {
-      this.destinationRoot('..');
-      this.appname = '';
+        this.argument('name', {
+            desc: 'Name of Dictionary',
+            type: String,
+            required: true,
+        });
     }
-  }
 
-  async prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the VS Code Spelling Dictionary Extension generator!'
-    ));
-
-    const prompts = [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Your extension dictionary name (i.e. medicalterms)',
-        default: this.options.name,
-        filter: name => name.toLowerCase().replace(/[^a-z0-9-]/g, '-')
-      },
-      {
-        type: 'input',
-        name: 'friendlyName',
-        message: 'Friendly Name',
-        default: props => title(props.name)
-      },
-      {
-        type: 'input',
-        name: 'displayName',
-        message: 'Display Name',
-        default: props => `${title(props.friendlyName)} - Code Spell Checker`
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Description',
-        default: props => `${title(props.friendlyName)} dictionary extension for VS Code.`
-      },
-      {
-        type: 'input',
-        name: 'dictionarySrc',
-        message: 'Source cspell-dicts Dictionary NPM name. To be installed. i.e. @cspell/dict-name',
-        default: props => `@cspell/dict-${props.name}`
-      },
-      {
-        type: 'confirm',
-        name: 'addCommands',
-        message: 'Add Enable / Disable Commands?',
-        default: true
-      },
-      {
-        type: 'input',
-        name: 'commandName',
-        message: 'Base Name for Commands',
-        default: props => title(props.name).replace(/[^a-z0-9]/gi, '_'),
-        when: props => props.addCommands
-      },
-      {
-        type: 'input',
-        name: 'locale',
-        message: 'Language Locale (i.e. "en" for English or "fr" for French)',
-        default: props => props.dictionarySrc.replace(/.*dict-/, '').slice(0, 2).toLowerCase(),
-        when: props => props.addCommands
-      },
-      {
-        type: 'input',
-        name: 'target',
-        message: 'Target Directory',
-        default: props => `extensions/${props.name}`
-      },
-      {
-        type: 'input',
-        name: 'fullPackageName',
-        message: 'NPM Package Name',
-        default: props => `code-spell-checker-${props.name}`
-      }
-    ];
-
-    this.props = await this.prompt(prompts);
-  }
-
-  writing() {
-    const files = [
-      'package.json',
-      'README.md',
-      'CHANGELOG.md',
-      'cspell-ext.json',
-      'src/extension.ts',
-      ['samples', 'samples'],
-      'LICENSE'
-    ];
-    const filesToCopy = [
-      ['.vscode', '.vscode'],
-      ['images', 'images'],
-      ['test', 'test'],
-      '.gitignore',
-      '.vscodeignore',
-      'tsconfig.json'
-    ];
-    files
-      .map(fromTo => typeof fromTo === 'string' ? [fromTo, fromTo] : fromTo)
-      .forEach(fromTo => {
-        const [src, dst] = fromTo;
-        this.fs.copyTpl(
-          this.templatePath(src),
-          this.destinationPath(dst),
-          this.props
-        );
-      });
-    filesToCopy
-      .map(fromTo => typeof fromTo === 'string' ? [fromTo, fromTo] : fromTo)
-      .forEach(fromTo => {
-        const [src, dst] = fromTo;
-        this.fs.copy(
-          this.templatePath(src),
-          this.destinationPath(dst)
-        );
-      });
-  }
-
-  default() {
-    const dstDir = this.props.target;
-    if (path.basename(this.destinationPath()) !== this.props.name) {
-      this.log(
-        'Creating Folder: ' + this.props.name
-      );
-      mkdirp(this.destinationPath(dstDir));
-      this.destinationRoot(this.destinationPath(dstDir));
+    async initializing() {
+        if (path.basename(this.destinationPath()) === genName) {
+            this.destinationRoot('..');
+            this.appname = '';
+        }
     }
-  }
 
-  install() {
-    if (this.props.dictionarySrc) {
-        this.spawnCommandSync('npm', ['install', '-S', this.props.dictionarySrc]);
-        this.spawnCommandSync('npx', ['prettier', '-w', "**/*.{yaml,json,ts,md}"]);
+    async prompting() {
+        // Have Yeoman greet the user.
+        this.log(yosay('Welcome to the VS Code Spelling Dictionary Extension generator!'));
+
+        const prompts = [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Your extension dictionary name (i.e. medicalterms)',
+                default: this.options.name,
+                filter: (name) => name.toLowerCase().replace(/[^a-z0-9-]/g, '-'),
+            },
+            {
+                type: 'input',
+                name: 'friendlyName',
+                message: 'Friendly Name',
+                default: (props) => title(props.name),
+            },
+            {
+                type: 'input',
+                name: 'displayName',
+                message: 'Display Name',
+                default: (props) => `${title(props.friendlyName)} - Code Spell Checker`,
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Description',
+                default: (props) => `${title(props.friendlyName)} dictionary extension for VS Code.`,
+            },
+            {
+                type: 'input',
+                name: 'dictionarySrc',
+                message: 'Source cspell-dicts Dictionary NPM name. To be installed. i.e. @cspell/dict-name',
+                default: (props) => `@cspell/dict-${props.name}`,
+            },
+            {
+                type: 'confirm',
+                name: 'addCommands',
+                message: 'Add Enable / Disable Commands?',
+                default: true,
+            },
+            {
+                type: 'input',
+                name: 'commandName',
+                message: 'Base Name for Commands',
+                default: (props) => title(props.name).replace(/[^a-z0-9]/gi, '_'),
+                when: (props) => props.addCommands,
+            },
+            {
+                type: 'input',
+                name: 'locale',
+                message: 'Language Locale (i.e. "en" for English or "fr" for French)',
+                default: (props) =>
+                    props.dictionarySrc
+                        .replace(/.*dict-/, '')
+                        .slice(0, 2)
+                        .toLowerCase(),
+                when: (props) => props.addCommands,
+            },
+            {
+                type: 'input',
+                name: 'target',
+                message: 'Target Directory',
+                default: (props) => `extensions/${props.name}`,
+            },
+            {
+                type: 'input',
+                name: 'fullPackageName',
+                message: 'NPM Package Name',
+                default: (props) => `code-spell-checker-${props.name}`,
+            },
+        ];
+
+        this.props = await this.prompt(prompts);
     }
-  }
 
-  end() {
-    this.spawnCommandSync('npm', ['install']);
-  }
+    writing() {
+        const files = [
+            'package.json',
+            'README.md',
+            'CHANGELOG.md',
+            'cspell-ext.json',
+            'src/extension.ts',
+            ['samples', 'samples'],
+            'LICENSE',
+        ];
+        const filesToCopy = [
+            ['.vscode', '.vscode'],
+            ['images', 'images'],
+            ['test', 'test'],
+            '.gitignore',
+            '.vscodeignore',
+            'tsconfig.json',
+        ];
+        files
+            .map((fromTo) => (typeof fromTo === 'string' ? [fromTo, fromTo] : fromTo))
+            .forEach((fromTo) => {
+                const [src, dst] = fromTo;
+                this.fs.copyTpl(this.templatePath(src), this.destinationPath(dst), this.props);
+            });
+        filesToCopy
+            .map((fromTo) => (typeof fromTo === 'string' ? [fromTo, fromTo] : fromTo))
+            .forEach((fromTo) => {
+                const [src, dst] = fromTo;
+                this.fs.copy(this.templatePath(src), this.destinationPath(dst));
+            });
+    }
+
+    async default() {
+        const dstDir = this.props.target;
+        if (path.basename(this.destinationPath()) !== this.props.name) {
+            this.log('Creating Folder: ' + this.props.name);
+            await mkdirp(this.destinationPath(dstDir));
+            this.destinationRoot(this.destinationPath(dstDir));
+        }
+    }
+
+    install() {
+        if (this.props.dictionarySrc) {
+            this.spawnCommandSync('npm', ['install', '-S', this.props.dictionarySrc]);
+            this.spawnCommandSync('npx', ['prettier', '-w', '**/*.{yaml,json,ts,md}']);
+        }
+    }
+
+    end() {
+        this.spawnCommandSync('npm', ['install']);
+    }
 };
 
 function title(s) {
-  return s[0].toUpperCase() + s.slice(1);
+    return s[0].toUpperCase() + s.slice(1);
 }
