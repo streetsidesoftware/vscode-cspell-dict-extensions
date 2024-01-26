7cc813d0eb7815eeebf765ec90d389218e0aed1f
Jason Dent <jason@streetsidesoftware.nl>
2024-01-26 18:04:53 +0100

    chore: Update the test runner to support ESM

--- test-runner/lib/runner.mjs
+++ test-runner/lib/runner.mjs
@@ -14,12 +14,14 @@ export async function run(testGlob, cwd) {
         color: true,
     });
 
-    const files = await glob('**/**.test.?(c)js', { cwd, absolute: true });
+    const files = await glob(testGlob, { cwd, absolute: true });
 
     console.log('files: %o', files);
 
     // Add files to the test suite
     files.forEach((f) => mocha.addFile(f));
+
+    await mocha.loadFilesAsync();
 
     return new Promise((resolve, reject) => {
         // Run the mocha test
--- test-runner/suite/index.cjs
+++ test-runner/suite/index.cjs
@@ -1,6 +1,6 @@
 async function run() {
     const { run: runRunner } = await import('../lib/runner.mjs');
-    await runRunner('**/**.test.?(c)js', __dirname);
+    await runRunner('**/**.test.?(m)js', __dirname);
 }
 
 module.exports = {
