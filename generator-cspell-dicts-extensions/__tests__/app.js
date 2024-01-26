52a102fdf13fc290bf073986b7c85478f2e672aa
Jason Dent <Jason3S@users.noreply.github.com>
2024-01-26 17:21:46 +0100

    chore: Update prettier (#856)

--- generator-cspell-dicts-extensions/__tests__/app.js
+++ generator-cspell-dicts-extensions/__tests__/app.js
@@ -4,15 +4,14 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-cspell-dicts:app', () => {
-  beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
-  });
+    beforeAll(() => {
+        return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({ someAnswer: true });
+    });
 
-  it('creates files', () => {
-    assert.file([
-      // cspell:ignore dummyfile
-      'dummyfile.txt'
-    ]);
-  });
+    it('creates files', () => {
+        assert.file([
+            // cspell:ignore dummyfile
+            'dummyfile.txt',
+        ]);
+    });
 });
