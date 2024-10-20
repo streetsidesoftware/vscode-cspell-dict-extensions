6c7bff434c529c5181cc4f0c7b5c1a428f7d8dde
Jason Dent <Jason3S@users.noreply.github.com>
2024-10-20 15:25:29 +0200

    fix: Add CSpell Bundled Dictionaries (#1003)

---
+++ extensions/cspell-bundled-dictionaries/README.md
@@ -0,0 +1,12 @@
+# CSpell Bundled Dictionaries for the Code Spell Checker
+
+The [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker) extension bundles the latest copy of the
+CSpell Dictionaries at the time the extension is published.
+
+This extension helps keep the dictionaries up to date by importing the latest CSpell bundled dictionaries from NPM.
+
+## Requirements
+
+This extension will automatically include [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker) extension.
+
+<!--- @@inject: ../../static/footer.md --->
---
+++ extensions/cspell-bundled-dictionaries/src/extension.ts
@@ -0,0 +1,31 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+interface CodeSpellCheckerExtension {
+    registerConfig(path: string): Promise<void>;
+    enableLocale(isGlobal: boolean, locale: string): Promise<void>;
+    disableLocale(isGlobal: boolean, locale: string): Promise<void>;
+}
+
+//
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    const vscodeSpellCheckerExtension = 'streetsidesoftware.code-spell-checker';
+    const configLocation = context.asAbsolutePath('./cspell-ext.json');
+
+    const extension = vscode.extensions.getExtension<CodeSpellCheckerExtension>(vscodeSpellCheckerExtension);
+
+    if (extension) {
+        extension.activate().then((ext) => {
+            // We need to register the dictionary configuration with the Code Spell Checker Extension
+            ext?.registerConfig?.(configLocation);
+        });
+    }
+
+    //
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
