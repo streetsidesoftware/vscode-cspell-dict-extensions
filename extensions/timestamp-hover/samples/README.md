8ca2db657751b5d28063f3eaf610d06ce6c75dc1
Jason Dent <jason@streetsidesoftware.nl>
2025-01-05 10:32:05 +0100

    feat: Add a simple extension to show timestamps

---
+++ extensions/timestamp-hover/samples/README.md
@@ -0,0 +1,9 @@
+# Medical Terms
+
+Some example medical terms.
+
+---
+
+abiana abiate abiatrophy abiatus abid abidi abidochromis abience abient abietene abietic abietine abietite abigeat abigei Abilify abinoxylan abio AbioCor abiogeneses abiogenesis abiogenetic abiogenic abiogenist abiogenous abiogeny Abiological Abiomed AbioMed abionergy abioseston abiosis abiotic abiotically
+
+ZUMI Zung Zunrisa Zuntz's Zuplenz Zweifel zwitterion Zyban Zybit Zyderm Zydis Zydone Zyflo zygal zygapophyseal zygapophyseales zygapophysial zygapophysiales zygapophysis zygia zygion Zygocotyle zygodactyly zygoma zygomas zygomatic zygomatica zygomatici zygomatico zygomaticoauricular zygomaticofacial zygomaticofacialis zygomaticofrontal zygomaticofrontalis zygomaticomaxillaris zygomaticomaxillary zygomaticosphenoid zygomaticotemporal zygomaticotemporalis
---
+++ extensions/timestamp-hover/src/extension.ts
@@ -0,0 +1,53 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    const options: FormatTimestampOptions = {
+        minDate: new Date('1980-01-01'),
+    }
+
+    const hoverProvider = vscode.languages.registerHoverProvider({ scheme: '*', language: '*' }, {
+        provideHover(document, position) {
+            const range = document.getWordRangeAtPosition(position);
+            if (!range) {
+                return undefined;
+            }
+            const word = document.getText(range);
+            const hoverMessage = formatTimestamp(word, options);
+            return hoverMessage ? new vscode.Hover(`_${hoverMessage}_`) : undefined;
+        }
+    });
+    context.subscriptions.push(hoverProvider);
+}
+
+const regexpIsNumber = /^\d+$/;
+
+interface FormatTimestampOptions {
+    minDate: Date;
+}
+
+function formatTimestamp(text: string, options: FormatTimestampOptions): string | undefined {
+    if (!regexpIsNumber.test(text)) {
+        return undefined;
+    }
+    let timestamp = Number(text);
+    if (timestamp < 1e11) {
+        // seconds to milliseconds
+        timestamp *= 1000;
+    }
+    if (timestamp > 1e14) {
+        // microseconds to milliseconds
+        timestamp /= 1000;
+    }
+    if (timestamp < options.minDate.getTime()) {
+        return undefined;
+    }
+    return new Date(timestamp).toISOString();
+}
+
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
